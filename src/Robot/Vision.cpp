0ef7e1ffbd7d012abd7157541f4c2b65b908b371
Rob <mail@robfors.com>
2017-11-15 10:29:52 -0500

    Adds suggestion.

--- src/Robot/Vision.cpp
+++ src/Robot/Vision.cpp
@@ -28,6 +28,23 @@ namespace Robot
   {
     
     Hardware::LIDARSensor* sensor = &Hardware::Main::lidar_sensor; // use this to access lidar
+    StepperMotor* motor = &Hardware::Main::lidar_stepper_motor; // use this to access motor
+    
+    //suggestions
+    
+    //rotate 'motor' to Angle 0
+    //rotate 'motor' to next angle
+    //wait for motor.is_stopped()
+    //get reading
+    //rotate 'motor' to next angle
+    //wait for motor.is_stopped()
+    //get reading
+    //rotate 'motor' to next angle
+    //wait for motor.is_stopped()
+    //get reading
+    //...
+    //rotate 'motor' to Angle 0
+    //repeat
     
     
   }
