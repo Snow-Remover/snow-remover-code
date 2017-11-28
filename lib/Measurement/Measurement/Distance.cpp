7eb8fc4e8decfbb091b8f4fd1b15a7659d0ec41d
Rob Fors <mail@robfors.com>
2017-11-28 10:23:08 -0500

    Fixes Measurement::Distance template methods.

--- lib/Measurement/Measurement/Distance.cpp
+++ lib/Measurement/Measurement/Distance.cpp
@@ -101,43 +101,6 @@ namespace Measurement
   }
   
   
-  template <typename Numeric>
-  Distance Distance::operator*(const Numeric number) const
-  {
-    return Distance::from_meters(meters() * number);
-  }
-  
-  
-  template <typename Numeric>
-  Distance& Distance::operator*=(const Numeric number)
-  {
-    _set_meters(meters() * number);
-    return *this;
-  }
-  
-  
-  template <typename Numeric>
-  Distance Distance::operator/(const Numeric number) const
-  {
-    return Distance::from_meters(meters() / number);
-  }
-  
-  
-  template <typename Numeric>
-  Distance& Distance::operator/=(const Numeric number)
-  {
-    _set_meters(meters() / number);
-    return *this;
-  }
-  
-  
-  template <typename Numeric>
-  Numeric Distance::operator/(const Distance& other_distance) const
-  {
-    return meters() / other_distance.meters();
-  }
-  
-  
   Distance Distance::operator%(Distance& other_distance) const
   {
     return Distance::from_meters(fmod(meters(), other_distance.meters()));
